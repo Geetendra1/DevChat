96441b805c765dbbc9882b7ecc327fde052e0c03
Geetendra <sengargeetendra123@gmail.com>
2020-02-08 10:05:02 +0530

    forth commit

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -17,11 +17,14 @@ class Messages extends Component {
     usersRef: firebase.database().ref('users'),
     messagesLoading: true,
     numUniqueUsers:'',
+    typingUsers:[],
     searchTerm: '',
     privateMessagesRef: firebase.database().ref('privateMessages'),
     searchLoading: false,
     searchResults: [],
-    isChannelStarred: false
+    isChannelStarred: false,
+    typingRef: firebase.database().ref('typing'),
+    connectedRef: firebase.database().ref('.info/connected')
   }
 
 
@@ -50,6 +53,40 @@ class Messages extends Component {
   
   addListners = channelId => {
     this.addMessageListner(channelId)
+    this.addTypingListner(channelId);
+  }
+
+  addTypingListner = channelId => {
+    let typingUsers = [];
+    this.state.typingRef.child(channelId).on('child_added',snap => {
+      if(snap.key != this.state.user.uid) {
+        typingUsers = typingUsers.concat({
+          id: snap.key,
+          name:snap.val()
+        })
+        this.setState({typingUsers});
+      }
+    })
+    this.state.typingRef.child(channelId).on('child_removed', snap => {
+      const index = typingUsers.findIndex(user => user.id === snap.key)
+      if(index !== -1) {
+        typingUsers = typingUsers.filter(user => user.id !== snap.key )
+        this.setState({typingUsers})
+      }
+    })
+    this.state.connectedRef.on('value', snap=> {
+      if(snap.val() === true) {
+        this.state.typingRef
+          .child(channelId)
+          .child(this.state.user.uid)
+          .onDisconnect()
+          .remove(err => {
+            if(err !== null) {
+              console.error(err)
+            }
+          })
+      }
+    })
   }
 
   addMessageListner = channelId => {
@@ -149,6 +186,18 @@ class Messages extends Component {
     return privateChannel ? privateMessagesRef : messagesRef;
   }
 
+  displayTypingUsers = users => (
+    users.length > 0 && users.map(user => (
+      <div style={{display:"flex", alignItems:"center", marginBottom:"0.2em"}}
+      key={user.id}
+       >
+         <span className="user__typing">{user.name} is typing</span><Typing/>
+
+      </div>
+    ))
+  )
+  
+
   handleSearchMessages = () => {
     const channelMessages = [...this.state.messages];
     const regex = new RegExp(this.state.searchTerm, 'gi')
@@ -169,7 +218,7 @@ class Messages extends Component {
 
   render() {
     const {messagesRef,channel,user, messages, numUniqueUsers, searchLoading,
-      searchTerm, searchResults, privateChannel, isChannelStarred} = this.state
+      searchTerm, searchResults, typingUsers, privateChannel, isChannelStarred} = this.state
     return (
      <React.Fragment>
        <MessagesHeader
@@ -185,9 +234,7 @@ class Messages extends Component {
        <Segment>
          <Comment.Group className="messages">
           {searchTerm ? this.displayMessages(searchResults) : this.displayMessages(messages)}
-          <div style={{display:"flex", alignItems:"center"}}>
-            <span className="user__typing">duglas is typing</span><Typing/>
-          </div>
+            {this.displayTypingUsers(typingUsers)}
          </Comment.Group>
        </Segment>
 
